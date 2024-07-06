6258e14211a8f74d12e6feb032fd9ff3857051b3
HP <sakshisukhani26@gmail.com>
2024-07-06 22:06:55 +0530

    save changes

--- lib/Player.dart
+++ lib/Player.dart
@@ -33,6 +33,11 @@ class _AudioPlayerWidgetState extends State<AudioPlayerWidget> {
   String formatTime(int seconds) {
     return '${(Duration(seconds: seconds))}'.split('.')[0].padLeft(8, '0');
   }
+  String formatDuration(Duration d){
+    final min=d.inMinutes.remainder(60);
+    final sec=d.inSeconds.remainder(60);
+    return "${min.toString().padLeft(2,'0')}:${sec.toString().padLeft(2,'0')}";
+  }
   @override
   void initState() {
     super.initState();
@@ -43,7 +48,6 @@ class _AudioPlayerWidgetState extends State<AudioPlayerWidget> {
       audioPlayer.onDurationChanged.listen((newDuration){
         setState(() {
           duration=newDuration;
-          log(newDuration as String);
         });
       });
       // audioPlayer.onDurationChanged.listen((Duration d) {
@@ -55,6 +59,9 @@ class _AudioPlayerWidgetState extends State<AudioPlayerWidget> {
         });
       });
     });
+  }
+  void handleSeek(double value){
+    audioPlayer.seek(Duration(seconds: value.toInt()));
   }
   @override
   void dispose() {
@@ -100,24 +107,25 @@ class _AudioPlayerWidgetState extends State<AudioPlayerWidget> {
               ClipRRect(borderRadius: BorderRadius.circular(20),
                 child: Image.network("https://i.scdn.co/image/ab67616d0000b273ba5db46f4b838ef6027e6f96",width: double.infinity,height: 350,fit: BoxFit.cover,),
               ),
-              // Slider(
-              //     min:0,
-              //     max: duration.inSeconds.toDouble(),
-              //     value: position.inSeconds.toDouble(),
-              //     onChanged: (value) async{}
-              //   // onChanged: (value)async{
-              //   //   final position=Duration(seconds: value.toInt());
-              //   //   audioPlayer.seek(position);
-              //   //   audioPlayer.resume();
-              //   // },
-              // ),
+              Slider(
+                  min:0,
+                  max: 100,
+                  value: position.inSeconds.toDouble(),
+                onChanged: handleSeek,
+                // onChanged: (value)async{
+                //   final position=Duration(seconds: value.toInt());
+                //   audioPlayer.seek(position);
+                //   audioPlayer.resume();
+                // },
+
+              ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 16),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Text(formatTime(position.inSeconds)),
-                    Text(formatTime(duration.inSeconds)),
+                    Text(formatTime((duration-position).inSeconds)),
                   ],
                 ),
               ),
